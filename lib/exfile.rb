8601f5c0f743a3a8a940bfd46c8036e3a045fd18
Eduardo Maia <eduvimaia@gmail.com>
2016-02-27 19:55:59 -0300

    initial import

---
+++ lib/exfile.rb
@@ -0,0 +1,29 @@
+module Exfile
+  autoload :Homebrew, "exfile/homebrew"
+
+  class << self
+    def osx?
+      RUBY_PLATFORM.downcase.include?("darwin")
+    end
+
+    def run(cmd)
+      puts "[Running] #{cmd}"
+      `#{cmd}` unless ENV['DEBUG']
+    end
+
+    def welcome_msg
+      puts
+      puts "======================================================"
+      puts "Welcome to EXFILES Installation."
+      puts "======================================================"
+      puts
+    end
+
+    def success_msg(action)
+      puts ""
+      puts "=========================================================="
+      puts ""
+      puts "EXFILES has been #{action}. Please restart your terminal and vim."
+    end
+  end
+end
---
+++ spec/exfile/exfile_spec.rb
@@ -0,0 +1,17 @@
+require "spec_helper"
+
+RSpec.describe Exfile do
+  subject { described_class }
+
+  let(:cmd_msg)     { "[Running] ls .\n" }
+  let(:welcome_msg) { /Welcome to EXFILES Installation/ }
+  let(:success_msg) { /EXFILES has been yup. Please restart your terminal and vim/ }
+
+  describe "base" do
+    it { expect(subject).to be_osx }
+
+    it { expect { subject.run("ls .") }.to output(cmd_msg).to_stdout }
+    it { expect { subject.welcome_msg }.to output(welcome_msg).to_stdout }
+    it { expect { subject.success_msg("yup") }.to output(success_msg).to_stdout }
+  end
+end
