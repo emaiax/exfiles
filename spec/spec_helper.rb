eafa52f5b7aa21f651a3858da2a531ab6caeabc7
Eduardo Maia <eduvimaia@gmail.com>
2016-02-29 13:01:35 -0300

    code coverage and codeclimate's reporter

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,13 @@
+if ENV["CI"]
+  require "codeclimate-test-reporter"
+  CodeClimate::TestReporter.start
+end
+
+unless ENV["NO_COVERAGE"]
+  require "simplecov"
+  SimpleCov.start { minimum_coverage "100" }
+end
+
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "exfile"
 
