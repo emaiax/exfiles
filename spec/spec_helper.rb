bc73d2f6482541bd0c20ea676faa41f243e0954a
Eduardo Maia <eduvimaia@gmail.com>
2016-02-29 23:08:26 -0300

    configure rspec to mock $?.success?

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,6 +12,12 @@ $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "exfile"
 
 RSpec.configure do |config|
+  # mocks the $?.success? to true/false
+  #
+  config.mock_with :rspec do |mocks|
+    mocks.allow_message_expectations_on_nil = true
+  end
+
   config.before(:all) { ENV["EXFILES_DEBUG"] = "true" }
   config.after(:all) { ENV["EXFILES_DEBUG"] = nil }
 end
