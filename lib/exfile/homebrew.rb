d1ac2518a66f836ffe76f449c0a62e28b38aadfb
Eduardo Maia <eduvimaia@gmail.com>
2016-02-29 23:09:17 -0300

    tests all methods on Exfile::Homebrew and uses brew bundle Brewfile

--- lib/exfile/homebrew.rb
+++ lib/exfile/homebrew.rb
@@ -9,21 +9,25 @@ module Exfile
         else
           install_homebrew
         end
+
+        bundle_brewfile
       end
 
       private
 
       def already_installed?
-        run %{which brew}
-        $?.success?
+        run %( which brew )
+        $CHILD_STATUS.success?
       end
 
       def install_homebrew
+        puts
+        puts
         puts "======================================================"
         puts "Installing Homebrew, the OSX package manager...If it's"
         puts "already installed, this will do nothing."
         puts "======================================================"
-        run %{ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"}
+        run %( ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" )
       end
 
       def update_homebrew
@@ -32,14 +36,18 @@ module Exfile
         puts "======================================================"
         puts "Updating Homebrew."
         puts "======================================================"
-        run %{brew update}
+        run %( brew update )
+      end
+
+      def bundle_brewfile
         puts
         puts
         puts "======================================================"
-        puts "Installing Homebrew packages...There may be some warnings."
+        puts "Installing Homebrew packages with Brewfile...There may be some warnings."
         puts "======================================================"
-        run %{brew install zsh ctags git hub tmux reattach-to-user-namespace the_silver_searcher}
-        run %{brew install macvim --custom-icons --override-system-vim --with-lua --with-luajit}
+        # run %{brew install zsh ctags git hub tmux reattach-to-user-namespace the_silver_searcher}
+        # run %{brew install macvim --custom-icons --override-system-vim --with-lua --with-luajit}
+        run %( brew bundle Brewfile )
         puts
         puts
       end
--- spec/exfile/exfile_homebrew_spec.rb
+++ spec/exfile/exfile_homebrew_spec.rb
@@ -3,15 +3,21 @@ require "spec_helper"
 RSpec.describe Exfile::Homebrew do
   subject { described_class }
 
-  describe "when homebrew is not installed" do
-    before { allow(subject).to receive(:already_installed?).and_return(false) }
+  let(:update_msg) { /Updating Homebrew/ }
+  let(:install_msg) { /Installing Homebrew, the OSX package manager/ }
+  let(:bundle_msg) { /Installing Homebrew packages with Brewfile\.\.\.There may be some warnings/ }
 
-    it { expect { subject.install }.to output(/Installing Homebrew, the OSX package manager/).to_stdout }
+  describe "when homebrew is not installed" do
+    before { allow($CHILD_STATUS).to receive(:success?).and_return(false) }
+
+    it { expect { subject.install }.to output(install_msg).to_stdout }
+    it { expect { subject.install }.to output(bundle_msg).to_stdout }
   end
 
   describe "when homebrew is already installed" do
-    before { allow(subject).to receive(:already_installed?).and_return(true) }
+    before { allow($CHILD_STATUS).to receive(:success?).and_return(true) }
 
-    it { expect { subject.install }.to output(/Updating Homebrew/).to_stdout }
+    it { expect { subject.install }.to output(update_msg).to_stdout }
+    it { expect { subject.install }.to output(bundle_msg).to_stdout }
   end
 end
