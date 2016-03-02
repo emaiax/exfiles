825425d6d0e08fdd270a3a86ab6fedd1bfda36ef
Eduardo Maia <eduvimaia@gmail.com>
2016-03-01 21:59:11 -0300

    there's no need to control xcode thru exfile for now

--- lib/exfile.rb
+++ lib/exfile.rb
@@ -1,11 +1,9 @@
 require_relative "exfile/base"
 require_relative "exfile/homebrew"
-require_relative "exfile/xcode"
 
 module Exfile
   autoload :Base,     "exfile/base"
   autoload :Homebrew, "exfile/homebrew"
-  autoload :Xcode,    "exfile/xcode"
 
   extend Exfile::Base
 end
