8b7845ba33add0b8392aa859dd51590ee83bd4eb
Eduardo Maia <eduvimaia@gmail.com>
2016-03-02 23:51:44 -0300

    creates fail_msg method with a link to opening issue on github to report what happened

--- lib/exfile/base.rb
+++ lib/exfile/base.rb
@@ -1,3 +1,6 @@
+require "uri"
+require_relative "version"
+
 module Exfile
   module Base
     def run(cmd)
@@ -18,10 +21,20 @@ module Exfile
     end
 
     def success_msg(action)
-      puts ""
+      puts
       puts "======================================================"
-      puts ""
+      puts
       puts "EXFILES has been #{action}. Please restart your terminal and vim."
+    end
+
+    def fail_msg(reason)
+      puts
+      puts "======================================================"
+      puts
+      puts "We're very sorry, but EXFILES couldn't be installed at this time </3"
+      puts "Please open an issue on Github following the link bellow:"
+      puts
+      puts URI.encode("https://github.com/emaiax/dotfiles/issues/new?labels=bug&title=[E#{Exfile::VERSION}] #{reason}")
     end
   end
 end
--- spec/exfile/exfile_base_spec.rb
+++ spec/exfile/exfile_base_spec.rb
@@ -7,6 +7,14 @@ RSpec.describe Exfile::Base do
   let(:welcome_msg) { /Welcome to EXFILES Installation/ }
   let(:success_msg) { /EXFILES has been yup. Please restart your terminal and vim/ }
 
+  let(:fail_msgs) do
+    [
+      /We're very sorry, but EXFILES couldn't be installed at this time/,
+      /Please open an issue on Github following the link bellow/,
+      %r{https:\/\/github\.com\/emaiax\/dotfiles\/issues\/new\?labels=bug&title=\[E0\.1\.0\]%20no%20reason}
+    ]
+  end
+
   describe "base" do
     context "on linux" do
       before { stub_const("Object::RUBY_PLATFORM", "x86_64-linux") }
@@ -23,5 +31,9 @@ RSpec.describe Exfile::Base do
     it { expect { subject.run("ls .") }.to output(cmd_msg).to_stdout }
     it { expect { subject.welcome_msg }.to output(welcome_msg).to_stdout }
     it { expect { subject.success_msg("yup") }.to output(success_msg).to_stdout }
+
+    it { expect { subject.fail_msg("no reason") }.to output(fail_msgs[0]).to_stdout }
+    it { expect { subject.fail_msg("no reason") }.to output(fail_msgs[1]).to_stdout }
+    it { expect { subject.fail_msg("no reason") }.to output(fail_msgs[2]).to_stdout }
   end
 end
