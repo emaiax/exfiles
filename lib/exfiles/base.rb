497d3460761e1d148c1a551e78fce25c2ee9ff98
Eduardo Maia <eduvimaia@gmail.com>
2020-05-06 00:51:52 -0300

    removed name and path, asking questions and added method to check linux

--- lib/exfiles/base.rb
+++ lib/exfiles/base.rb
@@ -6,28 +6,32 @@ module Exfiles
   module Base
     extend self
 
-    def name
-      "EXFILES"
-    end
+    def ask_to_install(section)
+      return true if ENV.fetch("ASK", "false") == "false"
 
-    def path
-      "~/.dotfiles"
+      puts "Would you like to install configuration files for: #{section}? [Y]es, [n]o"
+
+      STDIN.gets.chomp != "n"
     end
 
     def run(cmd)
       puts "[Running] #{cmd}"
 
-      !`#{cmd}`.empty? unless ENV["EXFILES_DEBUG"]
+      !`#{cmd}`.empty? unless ENV["DEBUG"]
     end
 
     def osx?
       RUBY_PLATFORM.downcase.include?("darwin")
     end
 
+    def linux?
+      RUBY_PLATFORM.downcase.include?("linux")
+    end
+
     def welcome
       puts
       puts "======================================================"
-      puts "Welcome to #{name} Installation. #{Tty.emoji}"
+      puts "Welcome to EXFILES Installation. #{Tty.emoji}"
       puts "======================================================"
       puts
     end
@@ -36,13 +40,15 @@ module Exfiles
       puts
       puts "======================================================"
       puts
-      puts "#{name} has been installed successfully. Please restart your terminal and vim. #{Tty.emoji}"
+      puts "EXFILES has been successfully installed to ~/.dotfiles"
+      puts "Please restart your terminal and vim."
+      puts "Cheers #{Tty.emoji}"
     end
 
     def fail(reason)
       puts "======================================================"
       puts
-      puts "We're very sorry, #{name} couldn't be installed at this time. #{Tty.broken_heart}"
+      puts "We're very sorry, EXFILES couldn't be installed at this time. #{Tty.broken_heart}"
       puts "Please open an issue on Github following the link below:"
       puts
       puts CGI.escape("https://github.com/emaiax/dotfiles/issues/new?labels=bug&title=[#{VERSION}] #{reason}")
--- spec/exfiles/base_spec.rb
+++ spec/exfiles/base_spec.rb
@@ -5,21 +5,38 @@ require "spec_helper"
 RSpec.describe Exfiles::Base do
   let(:cmd_msg)     { "[Running] ls .\n" }
   let(:welcome_msg) { /Welcome to EXFILES Installation/ }
-  let(:success_msg) { /EXFILES has been installed successfully\. Please restart your terminal and vim/ }
+  let(:success_msg) { /EXFILES has been successfully installed to/ }
 
   let(:fail_msgs) { /We're very sorry, EXFILES couldn't be installed at this time/ }
+
+  describe "ask" do
+    context "auto install" do
+      it { expect(subject.ask_to_install("whatever")).to be_truthy }
+    end
+
+    context "ask to install" do
+      before { ENV["ASK"] = "true" }
+      before { allow(STDIN).to receive(:gets) { "y" } }
+
+      it { expect(subject.ask_to_install("whatever")).to be_truthy }
+
+      after { ENV["ASK"] = nil }
+    end
+  end
 
   describe "base" do
     context "on osx" do
       before { stub_osx }
 
       it { expect(subject).to be_osx }
+      it { expect(subject).to_not be_linux }
     end
 
     context "on linux" do
       before { stub_linux }
 
       it { expect(subject).to_not be_osx }
+      it { expect(subject).to be_linux }
     end
 
     it { expect { subject.run("ls .") }.to output(cmd_msg).to_stdout }
